746d992bcc63bd476c671cd05e0526f42f964c5b
Minhaj Arif <minhajarif098@gmail.com>
2025-12-13 19:03:36 +0530

    Update engineer.js

--- explore-training&careerguidence/engineer.js
+++ explore-training&careerguidence/engineer.js
@@ -1,1 +1,12 @@
-.
+const exploreLi = document.getElementById("exploreLi");
+const exploreDropdown = document.getElementById("exploreDropdown");
+
+if (exploreLi && exploreDropdown) {
+  exploreLi.addEventListener("mouseenter", () => {
+    exploreDropdown.style.display = "block";
+  });
+
+  exploreLi.addEventListener("mouseleave", () => {
+    exploreDropdown.style.display = "none";
+  });
+}
