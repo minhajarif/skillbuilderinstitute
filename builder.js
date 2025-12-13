a4f06785c9910ebff6c9121c2fe276e5f947a901
Minhaj Arif <minhajarif098@gmail.com>
2025-12-13 12:58:15 +0530

    Update builder.js

--- builder.js
+++ builder.js
@@ -7,3 +7,14 @@ setInterval(() => {
   i = (i + 1) % slides.length;
   slides[i].classList.add('active');
 }, 5000);
+
+/* slider auto – home page only */
+let slides=document.querySelectorAll('.slide');
+let i=0;
+if(slides.length){
+  setInterval(()=>{
+    slides[i].classList.remove('active');
+    i=(i+1)%slides.length;
+    slides[i].classList.add('active');
+  },5000);
+}
